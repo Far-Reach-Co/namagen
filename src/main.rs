acc60c81d13aa4f1f17824be94a9c1862bd8bc85
kaBeech <contact@kaBeech.com>
2023-03-29 20:47:27 -0700

    Implement language list verification

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
     println!("Choose a language to generate a name in!");
-    println!("Please input the name of a language");
-    println!("Input 'List' for a list of available langauges");
+    println!("Please enter the name of a language");
+    println!("Enter 'List' for a list of available languages");
 
     let mut current_language = String::new();
+
+    let language_list = ["Saurian", "Wyr"];
 
     io::stdin()
         .read_line(&mut current_language)
         .expect("Failed to read line");
 
-    println!("Generating a name in the {current_language} language...")
+    let current_language: &str = &current_language.trim();
+
+    match current_language {
+        "List" => println!("{:?}", language_list),
+        "Saurian" => println!("Generating a name in the {current_language} language..."),
+        "Wyr" => println!("Generating a name in the {current_language} language..."),
+        _ => println!("Language '{current_language}' not found! Enter 'List' for a list of available langauges"),
+    }
 }
