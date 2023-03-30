43058342d3ffb648fe8499f7b2986df9c2242b47
kaBeech <contact@kaBeech.com>
2023-03-29 17:43:01 -0700

    Add basic user interaction

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
+    println!("Choose a language to generate a name in!");
+    println!("Please input the name of a language");
+    println!("Input 'List' for a list of available langauges");
+
+    let mut current_language = String::new();
+
+    io::stdin()
+        .read_line(&mut current_language)
+        .expect("Failed to read line");
+
+    println!("Generating a name in the {current_language} language...")
 }
