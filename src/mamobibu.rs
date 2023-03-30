16f952a15e6ebd2f5a5945c317ed52be028ee474
kaBeech <contact@kaBeech.com>
2023-03-30 16:33:55 -0700

    Comment out  message generation text

--- src/mamobibu.rs
+++ src/mamobibu.rs
@@ -5,10 +5,10 @@ pub fn mamobibu() -> String {
     let vowel_inventory = "aeiou";
     let syllables_per_name = 4;
 
-    "Generating a name in the Mamobibu language: ".to_owned()
-        + &generate_syllable_based_name(
-            consonant_inventory.to_owned(),
-            vowel_inventory.to_owned(),
-            syllables_per_name,
-        )
+    // "Generating a name in the Mamobibu language: ".to_owned()
+    generate_syllable_based_name(
+        consonant_inventory.to_owned(),
+        vowel_inventory.to_owned(),
+        syllables_per_name,
+    )
 }
