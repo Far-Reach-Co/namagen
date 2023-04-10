d1604d7f8b307e24ec79e431ee6d85152e273d28
kaBeech <contact@kaBeech.com>
2023-04-10 13:13:57 -0700

    Implement saurian second letter generation

--- src/gen_saurian_name.rs
+++ src/gen_saurian_name.rs
@@ -1,5 +1,5 @@
 use crate::gen_simple_syllable::gen_simple_syllable;
-use rand::seq::SliceRandom;
+use rand::{random, seq::SliceRandom};
 
 pub fn gen_saurian_name() -> String {
     let consonant_inventory = vec![
@@ -14,6 +14,8 @@ pub fn gen_saurian_name() -> String {
     let mut generated_name = String::from("");
 
     let mut most_recent_grapheme = "";
+    let mut two_vowels_or_consonants_in_a_row = false;
+    let mut generated_syllables = 0;
 
     let random_consonant: String = consonant_inventory
         .choose(&mut rng)
@@ -32,6 +34,28 @@ pub fn gen_saurian_name() -> String {
         generated_name.push_str(&random_vowel);
         most_recent_grapheme = "vowel";
     };
+
+    if random() {
+        let random_vowel: String = vowel_inventory.choose(&mut rng).unwrap().to_string();
+        generated_name.push_str(&random_vowel);
+        if most_recent_grapheme == "vowel" {
+            two_vowels_or_consonants_in_a_row = true
+        } else {
+            most_recent_grapheme = "vowel";
+        }
+    } else {
+        let random_consonant: String = consonant_inventory
+            .choose(&mut rng)
+            .unwrap()
+            .to_string()
+            .to_uppercase();
+        generated_name.push_str(&random_consonant);
+        if most_recent_grapheme == "consonant" {
+            two_vowels_or_consonants_in_a_row = true
+        } else {
+            most_recent_grapheme = "consonant";
+        }
+    }
 
     generated_name.to_string()
 }
