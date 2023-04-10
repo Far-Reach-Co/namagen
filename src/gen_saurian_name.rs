cff4eb02b0f91ec079ee78d82b7c1a088e9e7fec
kaBeech <contact@kaBeech.com>
2023-04-10 12:19:46 -0700

    Fix saurian consonant inventory

--- src/gen_saurian_name.rs
+++ src/gen_saurian_name.rs
@@ -1,7 +1,10 @@
 use crate::gen_syllable_based_name::gen_syllable_based_name;
 
 pub fn gen_saurian_name() -> String {
-    let consonant_inventory = vec!["'", "b", "j", "d", "h", "w", "z", "ħ", "ṭ" "y", "k", "l", "m", "n", "s", "ƹ", "f", "ṣ", "q", "r", "š", "t", "θ", "x", "ð", "ḍ", "ẓ", "ɣ"];
+    let consonant_inventory = vec![
+        "'", "b", "j", "d", "h", "w", "z", "ħ", "ṭ", "y", "k", "l", "m", "n", "s", "ƹ", "f", "ṣ",
+        "q", "r", "š", "t", "θ", "x", "ð", "ḍ", "ẓ", "ɣ",
+    ];
     let vowel_inventory = vec!["a", "i", "u"];
     let syllables_per_name = 3;
 
