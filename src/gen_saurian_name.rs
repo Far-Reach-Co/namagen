7bd93edff3b80e6955999c577a76cdfe398a236c
kaBeech <contact@kaBeech.com>
2023-04-10 11:22:22 -0700

    Adjust Saurian orthography

--- src/gen_saurian_name.rs
+++ src/gen_saurian_name.rs
@@ -1,7 +1,7 @@
 use crate::generate_syllable_based_name::generate_syllable_based_name;
 
 pub fn gen_saurian_name() -> String {
-    let consonant_inventory = vec!["'", "b", "j", "d", "h", "w", "z", "ħ", "ṭ" "y", "k", "l", "m", "n", "s", "3", "f", "ṣ", "q", "r", "š", "t", "θ", "x", "ð", "ḍ", "ẓ", "ɣ"];
+    let consonant_inventory = vec!["'", "b", "j", "d", "h", "w", "z", "ħ", "ṭ" "y", "k", "l", "m", "n", "s", "ƹ", "f", "ṣ", "q", "r", "š", "t", "θ", "x", "ð", "ḍ", "ẓ", "ɣ"];
     let vowel_inventory = vec!["a", "i", "u"];
     let syllables_per_name = 3;
 
