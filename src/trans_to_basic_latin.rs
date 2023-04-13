27ee3f0b12cc6da77ea5945d846c83cbe6c298c9
kaBeech <contact@kaBeech.com>
2023-04-13 14:10:42 -0700

    Add uppercase/lowercase conversions for first letter

--- src/trans_to_basic_latin.rs
+++ src/trans_to_basic_latin.rs
@@ -4,6 +4,14 @@ pub fn trans_to_basic_latin<'a>(
     alphabet: Vec<(&'static str, &'static str, i32)>,
 ) -> String {
     let mut word_basic_latin = word_to_transliterate.to_owned();
+
+    // Convert fist letter to lowercase
+    let mut chars = word_basic_latin.chars();
+    word_basic_latin = match chars.next() {
+        None => String::new(),
+        Some(char) => char.to_lowercase().collect::<String>() + chars.as_str(),
+    };
+
     for letter in alphabet {
         if letter.0 != letter.1 {
             word_basic_latin = word_basic_latin.replace(letter.0, letter.1)
@@ -26,5 +34,13 @@ pub fn trans_to_basic_latin<'a>(
     {
         word_basic_latin = word_basic_latin.replace("e", "a")
     }
+
+    // Convert fist letter to uppercase
+    chars = word_basic_latin.chars();
+    word_basic_latin = match chars.next() {
+        None => String::new(),
+        Some(char) => char.to_uppercase().collect::<String>() + chars.as_str(),
+    };
+
     word_basic_latin
 }
