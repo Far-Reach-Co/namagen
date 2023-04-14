8bdd1909b44b34539a83fc2cbfed4f1579cbf377
kaBeech <contact@kaBeech.com>
2023-04-13 17:33:47 -0700

    Update return formatting

--- src/controllers/generatorController.ts
+++ src/controllers/generatorController.ts
@@ -3,7 +3,8 @@ import { mamobibu, saurian } from "../../namagen/namagen.js";
 import { Context } from "https://deno.land/x/oak@v11.1.0/mod.ts";
 
 const genMamobibuName = (ctx: Context, next: Function) => {
-  ctx.response.body = { "result": mamobibu() };
+  const mamobibuName = mamobibu().split(":")[1];
+  ctx.response.body = { "mamobibuName": mamobibuName };
 };
 
 const genSaurianName = (ctx: Context, next: Function) => {
