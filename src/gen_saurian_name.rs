303ea8bb3e65ab242ec6e504efde2bc280804e39
kaBeech <contact@kaBeech.com>
2023-04-14 16:30:47 -0700

    Fix first letter bug temporarily

--- src/gen_saurian_name.rs
+++ src/gen_saurian_name.rs
@@ -19,36 +19,56 @@ pub fn gen_saurian_name() -> String {
     let vowel_inventory = assemble_grapheme_inventory(get_saurian_true_vowels());
     let mut rng = thread_rng();
     let syllables_per_name = rng.gen_range(1..4);
-    let mut most_recent_grapheme = "consonant";
+    let mut most_recent_grapheme = "";
     let mut two_vowels_or_consonants_in_a_row = false;
     let mut generated_syllables = 0;
     let mut generated_name = String::from("");
 
-    if most_recent_grapheme != "consonant" {
+    if most_recent_grapheme != "" {
         return "What???".to_string();
     }
 
-    // Generate first letter
-    if random() {
-        generated_name.push_str(
-            &consonant_inventory
-                .choose(&mut rng)
-                .unwrap()
-                .to_string()
-                .to_uppercase(),
-        );
-        most_recent_grapheme = "consonant";
+    // While loop is a temporary workaround for missing an uppercase letter at start of word
+    while generated_name == "" {
+        // Generate first letter
+        if random() {
+            generated_name.push_str(
+                &consonant_inventory
+                    .choose(&mut rng)
+                    .unwrap()
+                    .to_string()
+                    .to_uppercase(),
+            );
+            most_recent_grapheme = "consonant";
 
-        if generated_name == "'" {
-            // if random() && random() {
-            //     generated_name.push_str(
-            //         &vowel_and_syllabic_inventory
-            //             .choose(&mut rng)
-            //             .unwrap()
-            //             .to_string()
-            //             .to_uppercase(),
-            //     );
-            // } else {
+            if generated_name == "'" {
+                // if random() && random() {
+                //     generated_name.push_str(
+                //         &vowel_and_syllabic_inventory
+                //             .choose(&mut rng)
+                //             .unwrap()
+                //             .to_string()
+                //             .to_uppercase(),
+                //     );
+                // } else {
+                generated_name = vowel_inventory
+                    .choose(&mut rng)
+                    .unwrap()
+                    .to_string()
+                    .to_uppercase();
+                // }
+                most_recent_grapheme = "vowel";
+            };
+        // } else if random() && random() {
+        //     generated_name.push_str(
+        //         &vowel_and_syllabic_inventory
+        //             .choose(&mut rng)
+        //             .unwrap()
+        //             .to_string()
+        //             .to_uppercase(),
+        //     );
+        //     most_recent_grapheme = "vowel";
+        } else {
             generated_name.push_str(
                 &vowel_inventory
                     .choose(&mut rng)
@@ -56,30 +76,9 @@ pub fn gen_saurian_name() -> String {
                     .to_string()
                     .to_uppercase(),
             );
-            // }
             most_recent_grapheme = "vowel";
-        };
-    // } else if random() && random() {
-    //     generated_name.push_str(
-    //         &vowel_and_syllabic_inventory
-    //             .choose(&mut rng)
-    //             .unwrap()
-    //             .to_string()
-    //             .to_uppercase(),
-    //     );
-    //     most_recent_grapheme = "vowel";
-    } else {
-        generated_name.push_str(
-            &vowel_inventory
-                .choose(&mut rng)
-                .unwrap()
-                .to_string()
-                .to_uppercase(),
-        );
-        most_recent_grapheme = "vowel";
+        }
     }
-    let test = generated_name + " " + most_recent_grapheme + "k";
-    return test;
 
     // If first letter is a consonant, add second letter as a vowel (so we don't have names that start with two consonants like "Tdeneb")
     if most_recent_grapheme == "consonant" {
